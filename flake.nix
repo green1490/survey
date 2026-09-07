67a9c455a352197db38ffce3fcd27c8037d9c14d
green1490 <zambokrisztian1490@gmail.com>
2026-09-07 19:19:32 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Nix develop env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        opentofu
+        google-cloud-sdk
+        podman
+      ];
+    };
+  };
+}
